c5cf00b9f854a9272258ebfc042f8f3abe9e2f57
divya-yadamreddi <dyadamre@asu.edu>
2018-04-16 22:05:17 -0700

    Update tokenizer.pl

--- src/compiler/tokenizer.pl
+++ src/compiler/tokenizer.pl
@@ -1,28 +1,38 @@
-% @authors {Harshitha}
+% @authors {Harshitha}, @version 1
+% @authors {Divya}, @version 1.1
 % @purpose Tokenizer with space, newline and end of file delimiters
-% @version 1
-% @data 04/14/2018
+% @date 04/16/2018
 
 % Read the program from a file and returns the list of tokens
-tokenizer(FileName, Tokens) :- open(FileName, read, InStream),
-                      readTokens(InStream, Tokens),
+readProgram(FileName, Tokens) :- open(FileName, read, InStream),
+                      readWords(InStream, TokenCodes),
+                      tokenize(TokenCodes, Tokens),
                       close(InStream).
 
 % Return list of tokens to tokenizer
-readTokens(InStream,[]) :- at_end_of_stream(InStream), !.
-readTokens(InStream, [Token | RemTokens]) :- readToken(InStream, Token),
-                                            readTokens(InStream, RemTokens).
-
+readWords(InStream,[]) :- at_end_of_stream(InStream), !.
+readWords(InStream, [TokenCode | RemTokens]) :- get_code(InStream, TokenCode),
+                                            readWords(InStream, RemTokens).
 
 % Returns one token at a time
-readToken(InStream, Token) :- get_code(InStream, CharCode),
-                        checkTokens(CharCode, TokenCode, InStream),
-                        atom_codes(Token, TokenCode).
+tokenize([], []).
+tokenize([CharCode | RestCodes], Tokens) :- char_type(CharCode, space), !,
+    tokenize(RestCodes, Tokens).
+
+tokenize([CharCode | CharCodes], [Word | Tokens]) :- char_type(CharCode, alnum), !,
+    make_word([CharCode | CharCodes], alnum, WordCodes, RestCodes),
+    name(Word, WordCodes),
+    tokenize(RestCodes, Tokens).
+
+tokenize([CharCode | CharCodes], [Char | Tokens]) :- !,
+    name(Char, [CharCode]),
+    tokenize(CharCodes, Tokens).
 
 % Checks if next character is new line or space or end of file and returns token
 % upto that point
-checkTokens(10, [], _) :- !.
-checkTokens(32, [], _) :- !.
-checkTokens(end_of_file, [], _) :- !.
-checkTokens(CharCode, [CharCode|TokenCode], InStream) :- get_code(InStream, NextCharCode),
-                  checkTokens(NextCharCode, TokenCode, InStream).
+make_word([CharCode1, CharCode2 | CharCodes], alnum, [CharCode1 | WordCodes], RestCodes) :-
+    char_type(CharCode2, alnum),
+    !,
+    make_word([CharCode2 | CharCodes], alnum, WordCodes, RestCodes).
+
+make_word([CharCode | RestCodes], alnum, [CharCode], RestCodes).
