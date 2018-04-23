c7a6e64606ffe382c547ed45343ed9024f1a9f4b
kvsshirisha <saishirisha94@gmail.com>
2018-04-22 20:53:14 -0700

    Interpretar - added interpretar for while, condition and identifier

--- src/interpretar/Interpretar.pl
+++ src/interpretar/Interpretar.pl
@@ -1,6 +1,7 @@
 % @authors {Shirisha}, @version 1.0
+% @authors {Shirisha}, @version 1.1
 % @purpose Interpretar
-% @date 04/20/2018
+% @date 04/22/2018
 
 
 evalAssign(t_assignment(X,Y), EnvIn, EnvOut) :- evalExpression(Y, Output, EnvIn, EnvOut), update(X, Output, EnvIn, EnvOut).
@@ -25,3 +26,176 @@ evalWhile(t_while(X, _), EnvIn, EnvOut) :- /+ evalCondition(X, EnvIn, EnvOut), E
 
 
 
+evalCondition(t_singlecond(X, >, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                      evalExpression(Z, ExpOutput, EnvIn, EnvOut),
+
+                                                      IdOutput > ExpOut.
+
+evalCondition(t_singlecond(X, <, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                      evalExpression(Z, ExpOutput, EnvIn, EnvOut),
+
+                                                      IdOutput < ExpOut.
+
+evalCondition(t_singlecond(X, >=, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                      evalExpression(Z, ExpOutput, EnvIn, EnvOut),
+
+                                                      IdOutput >= ExpOut.
+
+evalCondition(t_singlecond(X, <=, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                      evalExpression(Z, ExpOutput, EnvIn, EnvOut),
+
+                                                      IdOutput <= ExpOut.
+
+evalCondition(t_singlecond(X, ==, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                      evalExpression(Z, ExpOutput, EnvIn, EnvOut),
+
+                                                      IdOutput =:= ExpOut.
+
+evalCondition(t_notcondition(not, X, >, Y), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                              evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                              IdOutput <= ExpOutput.
+
+evalCondition(t_notcondition(not, X, <, Y), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                              evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                              IdOutput >= ExpOutput.
+
+evalCondition(t_notcondition(not, X, >=, Y), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                              evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                              IdOutput < ExpOutput.
+
+evalCondition(t_notcondition(not, X, <=, Y), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                              evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                              IdOutput > ExpOutput.
+
+evalCondition(t_notcondition(not, X, ==, Y), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                              evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                              IdOutput /= ExpOutput.
+
+evalCondition(t_multiplecond(X, >, Y, and, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput > ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, <, Y, and, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput < ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, >=, Y, and, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput >= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, <=, Y, and, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput <= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, ==, Y, and, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput =:= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, >, Y, or, _), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                                IdOutput > ExpOutput.
+
+evalCondition(t_multiplecond(X, >, Y, or, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput <= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, <, Y, or, _), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                                IdOutput < ExpOutput.
+
+evalCondition(t_multiplecond(X, <, Y, or, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput >= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, >=, Y, or, _), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                                IdOutput >= ExpOutput.
+
+evalCondition(t_multiplecond(X, >=, Y, or, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput < ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, <=, Y, or, _), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                                IdOutput <= ExpOutput.
+
+evalCondition(t_multiplecond(X, <=, Y, or, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput > ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+evalCondition(t_multiplecond(X, ==, Y, or, _), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvOut),
+
+                                                                IdOutput =:= ExpOutput.
+
+evalCondition(t_multiplecond(X, ==, Y, or, Z), EnvIn, EnvOut) :- evalIdentifier(X, IdOutput, EnvIn, EnvIn2),
+
+                                                                evalExpression(Y, ExpOutput, EnvIn2, EnvIn3),
+
+                                                                IdOutput /= ExpOutput,
+
+                                                                evalCondition(Z, EnvIn3, EnvOut).
+
+
+
+evalIdentifier(t_identifier(X), Output, EnvIn, EnvIn) :- lookup(X, EnvIn, Output).
