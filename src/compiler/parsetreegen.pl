c2c9f0e9ffe173dfeb4013a33481d7db50e59c96
divya-yadamreddi <dyadamre@asu.edu>
2018-04-28 15:54:01 -0700

    Indentation

--- src/compiler/parsetreegen.pl
+++ src/compiler/parsetreegen.pl
@@ -5,42 +5,40 @@
 % @date 04/17/2018
 
 % Read the program from a file and returns the parse tree
-arrow(FileName) :- 	open(FileName, read, InStream),
-					tokenCodes(InStream, TokenCodes),
-					tokenize(TokenCodes, Tokens),
-					parser(ParseTree, Tokens, []),
-					close(InStream),
-					split_string(FileName, ".", "", L),
-					L = [H|_T],
-					atom_concat(H, ".ic", X),
-					open(X, write, OutStream),
-					write(OutStream, ParseTree),
-					write(OutStream, '.'),
-					close(OutStream).
-
+arrow(FileName) :- open(FileName, read, InStream),
+		   tokenCodes(InStream, TokenCodes),
+		   tokenize(TokenCodes, Tokens),
+		   parser(ParseTree, Tokens, []),
+		   close(InStream),
+		   split_string(FileName, ".", "", L),
+		   L = [H|_T],
+		   atom_concat(H, ".ic", X),
+		   open(X, write, OutStream),
+		   write(OutStream, ParseTree),
+		   write(OutStream, '.'),
+		   close(OutStream).
 
 % Return list of token codes to tokenizer
-tokenCodes(InStream,[]) :- 	at_end_of_stream(InStream), !.
+tokenCodes(InStream,[]) :- at_end_of_stream(InStream), !.
 tokenCodes(InStream, [TokenCode | RemTokens]) :- get_code(InStream, TokenCode),
                                                  tokenCodes(InStream, RemTokens).
 
 % Returns one token at a time
 tokenize([], []).
 tokenize([CharCode | RemCodes], Tokens) :- char_type(CharCode, space), !,
-										   tokenize(RemCodes, Tokens).
-tokenize([CharCode | CharCodes], [WordString | Tokens]) :- 	char_type(CharCode, alnum), !,
-															getWord([CharCode | CharCodes], alnum, WordCodes, RemCodes),
-															name(Word, WordCodes), atom_string(Word, WordString),
-															tokenize(RemCodes, Tokens).
+					   tokenize(RemCodes, Tokens).
+tokenize([CharCode | CharCodes], [WordString | Tokens]) :- char_type(CharCode, alnum), !,
+							   getWord([CharCode | CharCodes], alnum, WordCodes, RemCodes),
+							   name(Word, WordCodes), atom_string(Word, WordString),
+							   tokenize(RemCodes, Tokens).
 tokenize([CharCode | CharCodes], [CharString | Tokens]) :- !, name(Char, [CharCode]),
                                                            atom_string(Char, CharString),
-													       tokenize(CharCodes, Tokens).
+							   tokenize(CharCodes, Tokens).
 
 % Checks if next character is new line or space or end of file and returns token
 % upto that point
-getWord([CharCode1, CharCode2 | CharCodes], alnum, [CharCode1 | WordCodes], RemCodes) :-
-				char_type(CharCode2, alnum), !,
-				getWord([CharCode2 | CharCodes], alnum, WordCodes, RemCodes).
+getWord([CharCode1, CharCode2 | CharCodes], alnum, [CharCode1 | WordCodes], RemCodes) :-char_type(CharCode2, alnum), !,
+								getWord([CharCode2 | CharCodes], alnum, WordCodes, RemCodes).
 getWord([CharCode | RemCodes], alnum, [CharCode], RemCodes).
 
 
@@ -62,7 +60,6 @@ allstatements(t_declarationStatement(X)) --> declaration(X).
 allstatements(t_assign(X)) --> assign(X).
 allstatements(t_ifelseBlock(X)) --> ifelse(X).
 allstatements(t_whileBlock(X)) --> whileloop(X).
-
 
 % Rules for declarations
 declaration(t_singleDeclaration(X, Y, Z)) --> datatype(X), identifier(Y), ["="], data(Z), !.
@@ -86,33 +83,32 @@ factor(t_data(X)) --> data(X).
 
 % Rules for if-else
 ifelse(t_if(X, If)) --> ["if"], ["("], condition(X), [")"],
-												["{"], statements(If), [";"], ["}"].
+			["{"], statements(If), [";"], ["}"].
 
 ifelse(t_ifelseif(X, If, Y, Else)) --> ["if"], ["("], condition(X), [")"],
-																			 ["{"], statements(If), [";"], ["}"], [";"],
-																			 elseifLoop(Y), [";"],
-																			 ["else"], ["{"], statements(Else), [";"], ["}"].
+				       ["{"], statements(If), [";"], ["}"], [";"],
+				       elseifLoop(Y), [";"],
+				       ["else"], ["{"], statements(Else), [";"], ["}"].
 
 ifelse(t_ifelse(X,If,Else)) --> ["if"], ["("], condition(X), [")"],
-																["{"], statements(If), [";"], ["}"], [";"],
-															  ["else"], ["{"], statements(Else), [";"], ["}"].
+				["{"], statements(If), [";"], ["}"], [";"],
+				["else"], ["{"], statements(Else), [";"], ["}"].
 
 elseifLoop(t_elseifLoop(X, Y)) --> elseifLoop1(X), [";"], elseifLoop(Y).
 elseifLoop(t_elseifLoop(X)) --> elseifLoop1(X).
 elseifLoop1(t_elseifSingle(X, Elseif)) --> ["elseif"], ["("], condition(X), [")"],
-																				   ["{"], statements(Elseif), [";"], ["}"].
+					   ["{"], statements(Elseif), [";"], ["}"].
 
 % Rules for while
 whileloop(t_while(X,While)) --> ["while"], ["("],condition(X), [")"],
-																["{"] , statements(While), [";"], ["}"].
-
+				["{"] , statements(While), [";"], ["}"].
 
 % Rules for condition
 condition(t_singlecond(X, Y, Z)) --> identifier(X), comparision(Y), expression(Z).
 condition(t_multiplecond(X, Y, Z, P, Q)) --> identifier(X), comparision(Y),
-																						 expression(Z), condop(P), condition(Q).
+					     expression(Z), condop(P), condition(Q).
 condition(t_notcondition(X, Y, Z, P)) --> condnot(X), identifier(Y),
-																					comparision(Z), expression(P).
+					  comparision(Z), expression(P).
 condition(t_condition(true)) --> ["true"].
 condition(t_condition(false)) --> ["false"].
 
@@ -130,14 +126,13 @@ condnot(t_condnot(not)) -->["not"].
 
 % Rules for print
 printstatement(t_printComb(X, Y)) --> ["print"], ["'"], fullPrint(X), ["'"],
-																			["+"], identifier(Y), !.
+				      ["+"], identifier(Y), !.
 printstatement(t_printString(X)) --> ["print"], ["'"], fullPrint(X), ["'"], !.
 printstatement(t_printIdentifier(X)) --> ["print"], identifier(X), !.
 
 fullPrint(t_fullPrint(X)) --> singlePrint(X).
 fullPrint(t_fullPrint(X, Y)) --> singlePrint(X), fullPrint(Y).
 singlePrint(t_singlePrint(X)) --> [X], {string(X)}, !.
-
 
 % Rules for read
 readstatement(t_read(X)) --> identifier(X), ["="], ["read"].
